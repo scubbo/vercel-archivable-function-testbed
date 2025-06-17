bcd25e1d12db1bfb8202c5b68ea022193ac11d5a
Jack Jackson <scubbojj@gmail.com>
2025-06-17 09:31:44 -0700

    Retrigger

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,5 +2,6 @@
 export async function GET(_: Request) {
   const response = await fetch('https://api.vercel.app/products');
   const products = await response.json();
+  // No-op change to retrigger
   return Response.json(products);
 }
